9e8ce64ee28a05fe5b8d3d9804ab468d9e20517e
nikita <notgurev@gmail.com>
2022-03-20 23:37:17 +0300

    remove redundant ::text

--- src/script.sql
+++ src/script.sql
@@ -3,11 +3,11 @@
 
 \set name_tab '\'' :name_table '\''
 
-SELECT get_schemas(:name_tab::text);
+SELECT get_schemas(:name_tab);
 
 \echo 'Введите название схемы: '
 \prompt 'Введите название схемы: ' schema_name_cur
 
 \set name_shem '\'' :schema_name_cur '\''
 
-SELECT columns_info(:name_tab::text, :name_shem::text);
+SELECT columns_info(:name_tab, :name_shem);
