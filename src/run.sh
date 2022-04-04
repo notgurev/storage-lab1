6e702bf9f1387ccbba5003c06ac74ff7f3cc4d4a
nikita <notgurev@gmail.com>
2022-04-04 11:24:26 +0300

    remove debug echos

--- src/run.sh
+++ src/run.sh
@@ -12,26 +12,23 @@ IFS='.' read -r -a array <<<"$input"
 
 res="${input//[^\.]/}"
 if [[ "${#res}" == "2" ]]; then
-  echo "2"
   table="${array[2]}"
   schema="${array[1]}"
   db_="${array[0]}"
   if [ "$db" != "$db_" ]; then
-    echo "Database names don't match, exiting"
+    echo "Database names don't match, exiting..."
     exit
   fi
   export table
   export schema
   bash noprompt.sh "$db" "$schema" "$table"
 elif [[ "${#res}" == "1" ]]; then
-  echo "1"
   table="${array[1]}"
   schema="${array[0]}"
   export table
   export schema
   bash noprompt.sh "$db"
 elif [[ "${#res}" == "0" ]]; then
-  echo "0"
   export input
   bash prompt.sh "$db"
 else
